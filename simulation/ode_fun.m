cda1633875df1c6b5c0e7e35301e2fb07f122a55
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2018-11-15 20:40:03 -0300

    Adapted nested2d to new style: add sim_p, simple_smooth_trajectory and plot improvement

--- simulation/ode_fun.m
+++ simulation/ode_fun.m
@@ -1,4 +1,4 @@
-function x_dot = ode_fun(t,x,physics_p,control_p,traj_p)
+function x_dot = ode_fun(t, x, physics_p, control_p, traj_p, sim_p)
 
 % state renaming
 q = x(1:end/2);
@@ -8,10 +8,10 @@ q_dot = x(end/2+1:end);
 xref = traj_p.sample_fun(t);
 
 % control
-u = control_p.control_fun(q,q_dot,xref,physics_p,control_p);
+u = control_p.control_fun(q, q_dot, xref, physics_p, control_p);
 
 % dynamics
-q_ddot = physics_p.dyn_fun(q,q_dot,u,physics_p);
+q_ddot = sim_p.dyn_fun(q, q_dot, u, physics_p);
 
 % output build
 x_dot = x;
