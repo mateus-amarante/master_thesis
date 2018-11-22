d317cb0791d1e4c40fcdd71404ac3938405a21d2
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2018-11-21 22:46:56 -0300

    Minor changes to make smc2d and smc3d_slung work with new sim_p and trajectory definitions

--- simulation/config/smc3d_slung_config.m
+++ simulation/config/smc3d_slung_config.m
@@ -1,4 +1,4 @@
-function [physics_p, control_p, traj_p, plot_p] = smc3d_slung_config()
+function [physics_p, control_p, traj_p, sim_p, plot_p] = smc3d_slung_config()
 
 % Physical parameters
 physics_p = quadrotor3d_slung_physics();
@@ -27,7 +27,7 @@ control_p.n_input = 5;
 
 control_p.control_fun = @smc3d_slung_controller;
 
-% Trajectory parameters
+% Trajectory and simulation parameters
 xd = [0 0 2 2 0 0 0]';
 yd = [0 0 0 1 1 0 0]';
 zd = [0 0 1 1 0 0 0]';
@@ -38,23 +38,17 @@ pos = [xd yd zd psid];
 vel = zeros(size(pos));
 acc = zeros(size(pos));
 
-traj_p.Tf = 12;
-traj_p.dt = .02;
-traj_p.t = 0:traj_p.dt:traj_p.Tf;
-
 td = linspace(0,traj_p.Tf,length(xd));
 q = [pos vel acc];
 n_vars = size(pos,2);
 n_deriv_out = 3;
 
-% traj_p = simple3d_trajectory(xd,yd,zd,psid,T,steady_time,dt);
-% traj_p = simple3d_shaped_trajectory(xd,yd,zd,psid,T,steady_time,dt,physics_p);
 traj_p = shaped_poly_trajectory(td, q, n_vars, n_deriv_out, traj_p, physics_p);
 % traj_p.sample_fun = plan_polynomial_trajectory(td, q, n_vars, n_deriv_out);
-% traj_p.qd = traj_p.sample_fun(traj_p.t);
 
-traj_p.x0 = zeros(16,1);
-% traj_p.x0(6) = pi/2;
+sim_p.x0 = zeros(16, 1);
+sim_p.dyn_fun = @quadrotor3d_slung;
+% sim_p.t = 0:dt:(wait_time + T + steady_time);
 
 % Plot parameters
 plot_p.plot_state = @plot_quadrotor3d_slung_state;
