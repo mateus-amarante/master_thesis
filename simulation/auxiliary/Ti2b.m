6571bafa42be900e9cd68b7515665316c4a50925
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2018-12-02 16:34:12 -0300

    Removed old folders

--- simulation/auxiliary/Ti2b.m
+++
@@ -1,8 +0,0 @@
-function M = Ti2b(phi,theta)
-% Angular velocity transformation matrix
-% Inertial frame-> Body frame
-M=[ 1  0        -sin(theta);
-    0  cos(phi)  sin(phi)*cos(theta);
-    0 -sin(phi)  cos(phi)*cos(theta)];
-
-end
