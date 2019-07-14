0cec2f4140557d46bef2cae19bde29470eb54d66
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2019-07-14 18:29:25 -0300

    Update jupytext version and sync config

--- stability_slung_load_3d.py
+++ stability_slung_load_3d.py
@@ -1,12 +1,12 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:light
+#     formats: ipynb,py
 #     text_representation:
 #       extension: .py
 #       format_name: light
 #       format_version: '1.4'
-#       jupytext_version: 1.1.3
+#       jupytext_version: 1.1.7
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
