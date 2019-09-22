81d010f6318eeea404cf53180c5a2e352a8509c8
Mateus Amarante <mateus.amarujo@gmail.com>
2019-09-22 09:13:56 -0300

    Create polynomial trajectory planner based on spapi function

--- trajectory_planning/test_polynomial_trajectory.m
+++ trajectory_planning/test_polynomial_trajectory.m
@@ -14,7 +14,8 @@ accel = vel;
 
 % LINEAR
 x_d = pos;
-sample_fun = plan_polynomial_trajectory(t, x_d, 2, 3);
+sample_fun = plan_polynomial_trajectory2(t, x_d, 2, 2);
+%  sample_fun = plan_polynomial_trajectory(t, x_d, 2, 3);
 
 qqd = sample_fun(tt);
 
@@ -24,20 +25,22 @@ legend('pos1', 'pos2', 'vel1', 'vel2', 'acc1', 'acc2');
 
 % CUBIC
 x_d = [pos vel];
-sample_fun = plan_polynomial_trajectory(t, x_d, 2, 2);
+sample_fun = plan_polynomial_trajectory2(t, x_d, 2, 2);
+% sample_fun = plan_polynomial_trajectory(t, x_d, 2, 3);
 
 qqd = sample_fun(tt);
 
 figure;
 plot(tt,qqd);
-legend('pos1', 'pos2', 'vel1', 'vel2');
+legend('pos1', 'pos2', 'vel1', 'vel2', 'acc1', 'acc2');
 
 % QUINTIC
 x_d = [pos vel accel];
-sample_fun = plan_polynomial_trajectory(t, x_d, 2, 1);
+sample_fun = plan_polynomial_trajectory2(t, x_d, 2, 2);
+% sample_fun = plan_polynomial_trajectory(t, x_d, 2, 3);
 
 qqd = sample_fun(tt);
 
 figure;
 plot(tt,qqd);
-legend('pos1', 'pos2');
+legend('pos1', 'pos2', 'vel1', 'vel2', 'acc1', 'acc2');
