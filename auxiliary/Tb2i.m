b5d7670edfe84d399095503af474046d460fcbbc
Mateus Amarante <mateus.amarujo@gmail.com>
2019-09-30 21:06:58 -0300

    Fix wrong angle

--- auxiliary/Tb2i.m
+++ auxiliary/Tb2i.m
@@ -2,7 +2,7 @@ function M = Tb2i(phi,theta)
 % Angular velocity transformation matrix
 % Body frame-> Inertial frame
 M=[ 1  sin(phi)*tan(theta)   cos(phi)*tan(theta);
-    0  cos(phi)             -sin(theta);
+    0  cos(phi)             -sin(phi);
     0  sin(phi)/cos(theta)   cos(phi)/cos(theta)];
 
 end
