beb90300895b083e0791101c45c84428fc7f72ce
Mateus Amarante <mateus.amarujo@gmail.com>
2019-10-16 22:13:45 -0300

    Minor style update

--- auxiliary/Tb2i_dot.m
+++ auxiliary/Tb2i_dot.m
@@ -7,8 +7,8 @@ sphi = sin(phi);
 cphi = cos(phi);
 tgtheta = tan(theta);
 
-M=[ 0  sphi*thetadot/ctheta^2+cphi*tgtheta*phidot  -sphi*tgtheta*phidot+cphi*thetadot/ctheta^2;
-    0 -sphi*phidot                                 -cphi*phidot;
-    0  (sphi*tgtheta*thetadot+cphi*phidot)/ctheta   (-sphi*phidot+cphi*tgtheta*thetadot)/ctheta];
+M=[ 0,  sphi*thetadot/ctheta^2+cphi*tgtheta*phidot,  -sphi*tgtheta*phidot+cphi*thetadot/ctheta^2;
+    0, -sphi*phidot,                                 -cphi*phidot;
+    0,  (sphi*tgtheta*thetadot+cphi*phidot)/ctheta,  (-sphi*phidot+cphi*tgtheta*thetadot)/ctheta];
 
 end
