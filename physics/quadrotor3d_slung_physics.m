7e7c8efb9391d9b4390468d92b9197f278b95254
Mateus Amarante <mateus.amarujo@gmail.com>
2019-10-16 22:31:02 -0300

    Update physics parameters to match the DJI Matrice 100 drone

--- physics/quadrotor3d_slung_physics.m
+++ physics/quadrotor3d_slung_physics.m
@@ -1,21 +1,21 @@
-function [physics_p, dyn_fun] = quadrotor3d_slung_physics()
+function [physics_p] = quadrotor3d_slung_physics()
 
 % Physical parameters
-physics_p.M = 0.18;
-physics_p.Ix = 0.00025;
-physics_p.Iy = 0.000232;
-physics_p.Iz = 0.0003738;
+physics_p.M = 2.4;
+physics_p.Ix = 0.055;
+physics_p.Iy = 0.055;
+physics_p.Iz = 0.1;
 physics_p.I = diag([physics_p.Ix physics_p.Iy physics_p.Iz]);
 physics_p.Iinv = inv(physics_p.I);
 physics_p.g = 9.81;
 physics_p.kt = 8.54858e-06;
 physics_p.km = 1e-8;
-physics_p.r = .15;
+physics_p.r = .65/2;
 physics_p.maxThrust = 5*physics_p.M*physics_p.g;
-physics_p.load_radius = .04;
+physics_p.load_radius = .1;
 
-physics_p.m = physics_p.M*.5;
-physics_p.L = .4;
+physics_p.m = 1.0;
+physics_p.L = 1.0;
 physics_p.l = physics_p.L;
 
 physics_p.cx = .1;
