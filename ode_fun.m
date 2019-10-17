126de561b658eebcd862979a1856b4bce3561e7c
Mateus Amarante <mateus.amarujo@gmail.com>
2019-10-16 22:33:58 -0300

    Introduce state estimation noise

--- ode_fun.m
+++ ode_fun.m
@@ -1,10 +1,12 @@
 function xdot = ode_fun(t, x, physics_p, control_p, traj_p, sim_p)
 
+x_sensor = x + sim_p.noise(t);
+
 % trajectory sampling
 xref = traj_p.sample_fun(t);
 
 % control
-u = control_p.control_fun(x, xref, physics_p, control_p);
+u = control_p.control_fun(x_sensor, xref, physics_p, control_p);
 
 % dynamics
 xdot = sim_p.dyn_fun(x, u, physics_p);
