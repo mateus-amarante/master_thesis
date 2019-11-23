51267b49fc3eee821de8f07bcdfd49f73c2f35b5
Mateus Amarante <mateus.amarujo@gmail.com>
2019-11-23 11:49:00 -0300

    Update general plot configurations

--- startup.m
+++ startup.m
@@ -3,8 +3,12 @@ close all;
 clc;
 
 addpath('auxiliary','control','controllers','dynamics','physics',...
-    'config','plot','trajectories','trajectory_planning');
+    'config','plot','trajectories','trajectory_planning', 'analysis');
 
 set(groot,'defaulttextinterpreter','latex');  
 set(groot, 'defaultAxesTickLabelInterpreter','latex');  
-set(groot, 'defaultLegendInterpreter','latex'); 
+set(groot, 'defaultLegendInterpreter','latex');
+
+set(0, 'DefaultLineLineWidth', 1.25);
+set(0,'DefaultAxesFontSize',14)
+% set(0,'DefaultAxesTitleFontWeight','normal');
